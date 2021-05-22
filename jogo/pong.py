7771e63c375bed44ffaaf7308baca5baecd0be23
victor0machado <victor0machado@gmail.com>
2021-05-22 11:04:47 -0300

    Incluído renderização da tela e definição de fps

--- jogo/pong.py
+++ jogo/pong.py
@@ -1,5 +1,6 @@
 """Código principal do jogo."""
 from dataclasses import dataclass
+from typing import Tuple
 
 import pygame
 
@@ -11,6 +12,12 @@ class Tela:
     altura: int = 600
 
 
+@dataclass
+class Cores:
+    """Cores para a tela e objetos."""
+    branco: Tuple[int] = (255, 255, 255)
+
+
 def inicio_jogo() -> pygame.Surface:
     """Inicializa o jogo."""
     pygame.init()
@@ -20,6 +27,25 @@ def inicio_jogo() -> pygame.Surface:
     return tela
 
 
+def renderiza(tela: pygame.Surface) -> None:
+    """Renderiza a tela do jogo."""
+    tela.fill(Cores.branco)
+    pygame.display.update()
+
+
+def define_taxa_quadros(fps: int):
+    """Define a velocidade de execução do jogo em quadros por segundo."""
+    pygame.time.Clock().tick(fps)
+
+
+def roda_loop(tela: pygame.Surface) -> None:
+    """Roda o loop principal do jogo."""
+    while True:
+        renderiza(tela)
+        define_taxa_quadros(60)
+
+
 def jogo() -> None:
     """Entrada do jogo."""
     tela: pygame.Surface = inicio_jogo()
+    roda_loop(tela)
