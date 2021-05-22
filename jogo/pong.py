9da68d5a685584c0ea67f07d7f524ef9d8f28e4f
victor0machado <victor0machado@gmail.com>
2021-05-22 10:59:02 -0300

    Inicializando a tela do jogo

--- jogo/pong.py
+++ jogo/pong.py
@@ -1,6 +1,25 @@
 """Código principal do jogo."""
+from dataclasses import dataclass
+
+import pygame
+
+
+@dataclass
+class Tela:
+    """Informações sobre a tela."""
+    largura: int = 800
+    altura: int = 600
+
+
+def inicio_jogo() -> pygame.Surface:
+    """Inicializa o jogo."""
+    pygame.init()
+    tela = pygame.display.set_mode((Tela.largura, Tela.altura))
+    pygame.display.set_caption("Pong!")
+
+    return tela
 
 
 def jogo() -> None:
     """Entrada do jogo."""
-    print("Começou o jogo!")
+    tela: pygame.Surface = inicio_jogo()
